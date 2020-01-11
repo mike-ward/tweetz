38da6b13864b86603845b0dc57eef91fcbbe8124
Mike Ward <mike@mike-ward.net>
2020-01-11 10:24:37 -0600

    v2.0.beta.2

--- src/tweetz.core/Models/VersionInfo.cs
+++ src/tweetz.core/Models/VersionInfo.cs
@@ -2,7 +2,7 @@
 {
     public class VersionInfo
     {
-        public string Version => "v2.0.beta";
+        public string Version => "v2.0.beta.2";
         public string Copyright => "Copyright 2020, Mike Ward";
     }
 }
