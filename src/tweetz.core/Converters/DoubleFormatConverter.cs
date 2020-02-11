0190b64504916dbf721e032a377b2aff6791ff2e
mike-ward <mike@mike-ward.net>
2020-02-11 10:43:04 -0600

    change font selector to slider

--- src/tweetz.core/Converters/DoubleFormatConverter.cs
+++ src/tweetz.core/Converters/DoubleFormatConverter.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using System.Globalization;
-using System.Windows;
 using System.Windows.Data;
 
 namespace tweetz.core.Converters
 {
-    public class ResizeModeConverter : IValueConverter
+    public class DoubleFormatConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value is bool val && val
-                ? ResizeMode.NoResize
-                : ResizeMode.CanResize;
+            var format = parameter as string ?? "N";
+            return ((double)value).ToString(format, CultureInfo.InvariantCulture);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
