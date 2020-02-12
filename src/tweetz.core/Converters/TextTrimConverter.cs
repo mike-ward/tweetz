e9373edbc28d870f092da7dd8b001532fca77a44
mike-ward <mike@mike-ward.net>
2020-02-12 11:47:54 -0600

    Limit length of related link text to 300 characters

---
+++ src/tweetz.core/Converters/TextTrimConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace tweetz.core.Converters
+{
+    public class TextTrimConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var maxLength = 300;
+            var text = value.ToString();
+            return text!.Length > maxLength ? text.Substring(0, maxLength) + "…" : text;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
