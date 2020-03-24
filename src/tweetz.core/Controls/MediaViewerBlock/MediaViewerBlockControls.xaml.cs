76a0577df7fbb8e5bf08d0b054acff1efbaa94c0
Mike Ward <mike@mike-ward.net>
2020-03-23 21:15:01 -0500

    media player wip

--- src/tweetz.core/Controls/MediaViewerBlock/MediaViewerBlockControls.xaml.cs
+++ src/tweetz.core/Controls/MediaViewerBlock/MediaViewerBlockControls.xaml.cs
@@ -1,12 +1,50 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace tweetz.core.Controls.MediaViewerBlock
 {
     public partial class MediaViewerBlockControls : UserControl
     {
+        private DispatcherTimer timer;
+
         public MediaViewerBlockControls()
         {
             InitializeComponent();
+            Loaded += MediaViewerBlockControls_Loaded;
         }
+
+        private void MediaViewerBlockControls_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(0.25) };
+            timer.Tick += Timer_Tick;
+
+            MediaElement.MediaOpened += (_, __) => timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            if (MediaElement.NaturalDuration.HasTimeSpan)
+            {
+                Slider.Maximum = MediaElement.NaturalDuration.TimeSpan.TotalSeconds;
+                Slider.Value = MediaElement.Position.TotalSeconds;
+            }
+
+            if (MediaElement.Source == null)
+            {
+                timer.Stop();
+                Slider.Value = 0;
+            }
+        }
+
+        public MediaElement MediaElement
+        {
+            get { return (MediaElement)GetValue(MediaElementProperty); }
+            set { SetValue(MediaElementProperty, value); }
+        }
+
+        public static readonly DependencyProperty MediaElementProperty =
+            DependencyProperty.Register("MediaElement", typeof(MediaElement), typeof(MediaViewerBlockControls));
     }
 }
