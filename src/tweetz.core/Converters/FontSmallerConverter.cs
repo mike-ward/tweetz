851597a4cfc299fb041dd97f412844b724c083aa
mike-ward <mike@mike-ward.net>
2020-05-19 10:04:01 -0500

    related/qouted portions use proprotionally smaller font, added border back to related

---
+++ src/tweetz.core/Converters/FontSmallerConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace tweetz.core.Converters
+{
+    internal class FontSmallerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = value is double val
+                ? Math.Max(10, val - 0.5)
+                : value;
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
