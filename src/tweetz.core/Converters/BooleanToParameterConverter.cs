63c40ce710c99e724b89613f40ccc1b59ddd3aa6
mike-ward <mike@mike-ward.net>
2020-07-23 08:32:21 -0500

    rename converter

--- src/tweetz.core/Converters/BooleanToParameterConverter.cs
+++ src/tweetz.core/Converters/BooleanToParameterConverter.cs
@@ -4,7 +4,7 @@ using System.Windows.Data;
 
 namespace tweetz.core.Converters
 {
-    internal class BooleanToEffectConverter : IValueConverter
+    internal class BooleanToParameterConverter : IValueConverter
     {
         public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
