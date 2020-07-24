47c880b17cada28299ff655f9684ffcd35b7174b
mike-ward <mike@mike-ward.net>
2020-07-24 08:18:58 -0500

    #32 simpler still

--- src/tweetz.core/Infrastructure/Resources/GrayscaleShader.hlsl
+++ src/tweetz.core/Infrastructure/Resources/GrayscaleShader.hlsl
@@ -4,10 +4,10 @@ float4 main(float2 uv : TEXCOORD) : COLOR
 {
   float4 color = tex2D(implicitInput, uv);
 
-  float gray = sqrt(
-    color.r * color.r * .299 +
-    color.g * color.g * .587 +
-    color.b * color.b * .114);
+  float gray =
+    color.r * .299 +
+    color.g * .587 +
+    color.b * .114;
 
   color.r = gray;
   color.g = gray;
