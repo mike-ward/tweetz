1b4cf84e805910a72e5a1ef267192aaa9364af7b
mike-ward <mike@mike-ward.net>
2020-08-20 08:18:06 -0500

    #38 Add retry logic to image downloads

--- src/tweetz.core/Views/TweetBlock/TweetImageControl.xaml.cs
+++ src/tweetz.core/Views/TweetBlock/TweetImageControl.xaml.cs
@@ -1,11 +1,18 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 using tweetz.core.Services;
+using twitter.core.Models;
 
 namespace tweetz.core.Views
 {
     public partial class TweetImageControl : UserControl
     {
+        private const int MaxRetries = 3;
+        private int Retries { get; set; }
+
         public TweetImageControl()
         {
             InitializeComponent();
@@ -14,11 +21,37 @@ namespace tweetz.core.Views
         private void Image_ImageFailed(object sender, ExceptionRoutedEventArgs ea)
         {
             ea.Handled = true;
-            var image = (Image)sender;
-            var loadingIndicator = (TextBlock)image.Tag;
-            loadingIndicator.ToolTip = ea.ErrorException.Message;
-            loadingIndicator.Text = (string)Application.Current.FindResource("WarningSign");
-            TraceService.Message(ea.ErrorException.Message);
+            try
+            {
+                var image = (Image)sender;
+
+                if (Retries < MaxRetries)
+                {
+                    Retries += 1;
+                    var uri = new Uri(((Media)image.DataContext).MediaUrl);
+
+                    var bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+                    bitmap.UriSource = uri;
+                    bitmap.EndInit();
+
+                    image.Source = bitmap;
+                    TraceService.Message($"Retry ({Retries}) loading image: {uri}");
+                }
+                else
+                {
+                    var loadingIndicator = (TextBlock)image.Tag;
+                    loadingIndicator.ToolTip = ea.ErrorException.Message;
+                    loadingIndicator.Text = (string)Application.Current.FindResource("WarningSign");
+                    TraceService.Message(ea.ErrorException.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.ToString());
+                TraceService.Message(ex.Message);
+            }
         }
     }
 }
--- src/tweetz.core/Views/TweetBlock/TweetProfileImageControl.xaml.cs
+++ src/tweetz.core/Views/TweetBlock/TweetProfileImageControl.xaml.cs
@@ -3,11 +3,15 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 using tweetz.core.Services;
+using twitter.core.Models;
 
 namespace tweetz.core.Views
 {
     public partial class TweetProfileImageControl : UserControl
     {
+        private const int MaxRetries = 3;
+        private int Retries { get; set; }
+
         public TweetProfileImageControl()
         {
             InitializeComponent();
@@ -27,10 +31,29 @@ namespace tweetz.core.Views
         private void Image_ImageFailed(object sender, ExceptionRoutedEventArgs e)
         {
             e.Handled = true;
-            TraceService.Message(e.ErrorException.Message);
             var imageControl = (Image)sender;
-            var uri = new Uri("/Infrastructure/Resources/profile.png", UriKind.Relative);
-            imageControl.Source = new BitmapImage(uri);
+
+            if (Retries < MaxRetries)
+            {
+                Retries += 1;
+                var user = (User)imageControl.DataContext;
+                var uri = new Uri(user.ProfileImageUrl!);
+
+                var bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+                bitmap.UriSource = uri;
+                bitmap.EndInit();
+
+                imageControl.Source = bitmap;
+                TraceService.Message($"Retry ({Retries}) loading profile image: {uri}");
+            }
+            else
+            {
+                TraceService.Message(e.ErrorException.Message);
+                var uri = new Uri("/Infrastructure/Resources/profile.png", UriKind.Relative);
+                imageControl.Source = new BitmapImage(uri);
+            }
         }
     }
 }
