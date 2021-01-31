639242c2219cf36117bcb5e9566592d32ff3080a
Mike Ward <mike@mike-ward.net>
2021-01-30 19:32:28 -0600

    change link colors, extend timeout on reverse lookups

--- publish.cmd
+++ publish.cmd
@@ -2,25 +2,34 @@
 del /f/s/q bin > nul && rmdir /s/q bin
 if EXIST bin goto ERROR
 pushd src\tweetz.core
-
+ 
+echo(
+echo --- Build framework-dependent ---
+echo(
 dotnet publish -f net5.0-windows -r win10-x86 -c Release --self-contained false --output ../../bin/framework-dependent
 if ERRORLEVEL 1 goto ERROR
 
-dotnet publish -f net5.0-windows -r win10-x86 -c Release --self-contained true /p:PublishTrimmed=false --output ../../bin/self-contained
+echo(
+@echo --- Build self-contained ---
+echo(
+dotnet publish -f net5.0-windows -r win10-x86 -c Release --self-contained true --output ../../bin/self-contained
 if ERRORLEVEL 1 goto ERROR
 
 popd
-@echo Build Installer...
-iscc /Qp tweetz.core.setup.iss 
+echo(
+echo --- Build installer ---
+iscc /Q tweetz.core.setup.iss 
 if ERRORLEVEL 1 goto ERROR
 
 pushd bin
 
-@echo Build zip archives
-7z a -r dist/tweetz-framework-dependent.zip ./framework-dependent/*
+echo(
+echo(
+echo --- Build zip archives ---
+7z a -bso0 -bsp0 -r dist/tweetz-framework-dependent.zip ./framework-dependent/*
 if ERRORLEVEL 1 goto ERROR
 
-7z a -r dist/tweetz-self-contained.zip ./self-contained/*
+7z a -bso0 -bsp0 -r dist/tweetz-self-contained.zip ./self-contained/*
 if ERRORLEVEL 1 goto ERROR
 
 popd
