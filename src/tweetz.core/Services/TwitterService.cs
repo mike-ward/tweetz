aa91e8fb5809239c5442a6409a717b942ec44736
Mike Ward <mike@mike-ward.net>
2021-08-22 20:00:55 -0500

    refactor TwitterService further

--- src/tweetz.core/Services/TwitterService.cs
+++ src/tweetz.core/Services/TwitterService.cs
@@ -1,41 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Threading.Tasks;
 using tweetz.core.Interfaces;
-using twitter.core.Models;
 using twitter.core.Services;
 
 namespace tweetz.core.Services
 {
     public class TwitterService : ITwitterService
     {
-        private readonly ISettings  settings;
+        private ISettings  Settings   { get; }
         public  TwitterApi TwitterApi { get; }
 
         public TwitterService(ISettings settings)
         {
-            if (settings is null) throw new System.ArgumentNullException(nameof(settings));
-
             const string consumerKey    = "ZScn2AEIQrfC48Zlw";
             const string consumerSecret = "8gKdPBwUfZCQfUiyeFeEwVBQiV3q50wIOrIjoCxa2Q";
 
-            TwitterApi    = new TwitterApi(consumerKey, consumerSecret);
-            this.settings = settings;
+            Settings   = settings ?? throw new ArgumentNullException(nameof(settings));
+            TwitterApi = new TwitterApi(consumerKey, consumerSecret);
 
-            this.settings.PropertyChanged += delegate
+            Settings.PropertyChanged += delegate
             {
                 TwitterApi.AuthenticationTokens(
-                    this.settings.AccessToken,
-                    this.settings.AccessTokenSecret);
+                    Settings.AccessToken,
+                    Settings.AccessTokenSecret);
             };
         }
 
         public async ValueTask AuthenticateWithPinAsync(OAuthTokens requestTokens, string pin)
         {
             var access = await TwitterApi.AuthenticateWithPin(requestTokens, pin).ConfigureAwait(true);
-            settings.AccessToken       = access.OAuthToken;
-            settings.AccessTokenSecret = access.OAuthSecret;
-            settings.ScreenName        = access.ScreenName;
-            settings.Save();
+            Settings.AccessToken       = access.OAuthToken;
+            Settings.AccessTokenSecret = access.OAuthSecret;
+            Settings.ScreenName        = access.ScreenName;
+            Settings.Save();
         }
     }
 }
