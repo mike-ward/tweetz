fcc9312568eea3610f843ce4c9451da727375b93
mike-ward <mike@mike-ward.net>
2022-12-11 08:52:14 -0600

    v2022.5

--- src/tweetz.core/Models/VersionInfo.cs
+++ src/tweetz.core/Models/VersionInfo.cs
@@ -2,7 +2,7 @@
 {
     public static class VersionInfo
     {
-        public static string Version   => "v2022.4";
+        public static string Version   => "v2022.5";
         public static string Copyright => "Copyright 2022, Mike Ward";
     }
 }
